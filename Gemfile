89c7d9db92aa0adee72c9321db983ef646f085de
Chris Mytton <chrismytton@gmail.com>
2015-11-26 21:43:10 +0000

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,3 @@
+source 'https://rubygems.org'
+
+gem 'webhook_handler', '~> 0.4.0'
---
+++ app.rb
@@ -0,0 +1,9 @@
+require 'webhook_handler'
+
+class CountriesJsonUpdater
+  include WebhookHandler
+
+  def perform
+    # Your code goes here...
+  end
+end
