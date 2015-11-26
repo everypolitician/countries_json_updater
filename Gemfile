80f8fd8977c61e6f7f7daa91bd6b9de3dba592f9
Chris Mytton <chrismytton@gmail.com>
2015-11-26 21:57:18 +0000

    Add implementation

--- Gemfile
+++ Gemfile
@@ -1,3 +1,6 @@
 source 'https://rubygems.org'
 
 gem 'webhook_handler', '~> 0.4.0'
+gem 'puma'
+gem 'dotenv'
+gem 'everypoliticianbot', git: 'https://github.com/everypolitician/everypoliticianbot', branch: 'master'
--- app.rb
+++ app.rb
@@ -1,9 +1,43 @@
+require 'json'
+
 require 'webhook_handler'
+require 'everypoliticianbot'
+require 'dotenv'
+Dotenv.load
 
 class CountriesJsonUpdater
   include WebhookHandler
+  include Everypoliticianbot::Github
 
-  def perform
-    # Your code goes here...
+  begin
+    EVERYPOLITICIAN_DATA_REPO = ENV.fetch('EVERYPOLITICIAN_DATA_REPO')
+  rescue KeyError => e
+    abort "Missing required environment variable: #{e}"
+  end
+
+  def handle_webhook
+    request.body.rewind
+    payload = JSON.parse(request.body.read)
+    return unless payload['repository']['full_name'] == EVERYPOLITICIAN_DATA_REPO
+    return unless %w(opened synchronize).include?(payload['action'])
+    branch = payload['pull_request']['head']['ref']
+    self.class.perform_async(branch)
+  end
+
+  def perform(branch)
+    message = 'Refresh countries.json'
+    options = { branch: branch, message: message }
+    with_git_repo(everypolitician_data_repo, options) do
+      # Unset bundler environment variables so it uses the correct Gemfile etc.
+      env = {
+        'BUNDLE_GEMFILE' => nil,
+        'BUNDLE_BIN_PATH' => nil,
+        'RUBYOPT' => nil,
+        'RUBYLIB' => nil,
+        'NOKOGIRI_USE_SYSTEM_LIBRARIES' => '1'
+      }
+      system(env, 'bundle install')
+      system(env, 'bundle exec rake countries.json')
+    end
   end
 end
